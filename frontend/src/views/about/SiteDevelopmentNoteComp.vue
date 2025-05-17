82684755cb07a3284565a67822c9400b17dfabcc
M. Goto <ohkenxii@ohkenxii.local>
2025-05-17 16:32:45 +0900

    Update About coment

--- frontend/src/views/about/SiteDevelopmentNoteComp.vue
+++ frontend/src/views/about/SiteDevelopmentNoteComp.vue
@@ -10,11 +10,11 @@
       <div class="my-8">
         <li class="text-xl font-bold my-2 text-slate-800">Vue.js & Maplibre GL JS</li>
         <div class="leading-loose mx-6">
-          フロントエンドはVue.js（Vue 3 + Composition API）のSPA（Single Page
+          フロントエンドはVue.js（Vue 3 + Composition API）によるSPA（Single Page
           Application）で構築しています。 CSSフレームワークにはTailwind
-          CSSを採用し、保守性と堅牢性を高めるためにTypeScriptを導入し開発しています。
-          マップライブラリにはMaplibre GL
-          JSを使用し、背景地図としてOpenStreetMapとMapTilerを利用しています。
+          CSSを採用し、保守性と堅牢性の向上のためにTypeScriptを導入しています。
+          マップライブラリとしてはMaplibre GL
+          JSを使用し、背景地図にはOpenStreetMapおよびMapTilerを利用しています。
         </div>
       </div>
 
@@ -29,22 +29,23 @@
           >
             https://sites.google.com/view/eooxagrido/
           </a>
-          ）で公開されているマップを利用させて頂いてます。
-          公開されているGeoTIFFファイルを、GDALでCloud Optimized GeoTIFF（COG）化し、AWS
-          S3でホスティングしています。
-          フロントエンドでは、geotiff.jsを使用してブラウザから直接GeoTIFFを読み込み、fast-pngを用いてPNGタイルに変換・レンダリングしています。
+          ）にて公開されているGeoTIFFファイルを活用しています。 このファイルをGDALによりCloud
+          Optimized GeoTIFF（COG）形式へ変換し、AWS S3でホスティングしています。
+          フロントエンドでは、geotiff.jsを用いてブラウザから直接GeoTIFFを読み込み、fast-pngでPNGタイルに変換・描画しています。
         </div>
       </div>
 
       <div class="my-8">
-        <li class="text-xl font-bold my-2 text-slate-800">PMTiles</li>
+        <li class="text-xl font-bold my-2 text-slate-800">
+          PMTiles（Cloud Optimized Vector Tiles）
+        </li>
         <div class="leading-loose mx-6">
-          サイト上で圃場データを登録する際、農水省が公開している筆ポリゴン（圃場情報）も利用できる仕様としています。
-          筆ポリゴンのオリジナルファイルフォーマットはGeoJSONですが、 そのままMaplibre GL
-          JSで表示すると圃場数が多いため、レンダリング速度に課題があります。<br />
-          そこで、表示パフォーマスを向上させるために Cloud Optimized
-          Tiles（PMTiles）形式に変換しています。
-          タイルデータはtippecanoeを用いてGeoJSONからPMTilesファイルを生成し、COGと同様にAWS
+          圃場データの登録時に、農水省が公開している筆ポリゴン（圃場情報）を利用可能としています。
+          元データはGeoJSON形式ですが、 そのままMaplibre GL
+          JSで表示するとポリゴン数が多く描画パフォーマンスに課題があるため、
+          PMTiles形式に変換しています。
+          <br />
+          GeoJSONからの変換にはtippecanoeを使用し、生成されたPMTilesファイルはCOGと同様にAWS
           S3上でホスティングしています。
         </div>
       </div>
@@ -52,13 +53,13 @@
       <div class="my-8">
         <li class="text-xl font-bold my-2 text-slate-800">AWS CloudFront + S3 + Lambda</li>
         <div class="leading-loose mx-6">
-          本サイトは、Vue.jsで構築されたビルドファイルをAWS CloudFront および
-          S3によってホスティングしています。
-          可変施肥マップの作成においては、ユーザーが指定したパラメーターをAWS API
+          本サイトは、Vue.jsによるビルドファイルをAWS CloudFront および
+          S3によりホスティングしています。
+          可変施肥マップの作成に際しては、ユーザーが指定したパラメーターをAWS API
           Gateway経由で送信し、 AWS
-          Lambda（Pythonランタイム）によりでShapefile（シェープファイル）へと変換され、
-          クライアントに自動的にダウンロードされます。
-          サーバーレスアーキテクチャを採用することで、堅牢かつ低コストな運用を実現しています。<br />
+          Lambda（Pythonランタイム）にてShapefile（シェープファイル）に変換された後、
+          クライアント側に自動でダウンロードされます。
+          このように、サーバーレスアーキテクチャを採用することで、堅牢かつ低コストな運用を実現しています。<br />
         </div>
       </div>
 
@@ -66,7 +67,7 @@
         <li class="text-xl font-bold my-2 text-slate-800">Github & Github Actions</li>
         <div class="leading-loose mx-6">
           本サイトのソースコードはGithubで管理・公開しており、デプロイはGithub
-          Actionsを用いて自動化しています。 興味のある方はGithubリポジトリ（
+          Actionsによって自動化されています。 興味のある方はGithubリポジトリ（
           <a
             href="https://github.com/sindicum/vfmaker"
             target="_blank"
